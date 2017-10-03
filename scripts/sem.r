6a0024003bbacd0083b5e4a038762a4bcba53060
estvlei <32452938+estvlei@users.noreply.github.com>
2017-10-03 17:00:15 +0100

    Add files via upload

--- scripts/sem.r
+++ scripts/sem.r
@@ -1,6 +1,9 @@
-#sem.r
+### sem.r
+install.packages("mice")
+install.packages("semTools")
 library(mice)
 library(lavaan)
+library(semTools)
 
 wd <- getwd()
 datafile <- paste(wd, "RUNDMC_datasheet_long.csv", sep="/")
@@ -9,20 +12,26 @@ df <- read.csv(datafile, sep=";", dec=",")
 modelfile <- paste(wd, "model.lav", sep="/")
 model <- readLines(modelfile)
 
-variables06 <- c("wmh06", "HV_06_ml_norm", "cognitiveindex06_all", 
-                 "verbalmemory06_all",	"visuospatialmemory06_all",	
-                 "immediatememory06_all",	"delayedmemory06_all",	"workingmemory06_all",	
-                 "psychomotorspeed06_all",	"fluency06_all",	"responseinhibition06_all",	
-                 "attention06_all",	"executivefunction06_all")
-df06 <- df[variables06]
+variables06 <- c("wmh06", "hv06", "cognitiveindex06_all", "Inclusion")
+df06 <- df[variables06]                 
 
 fit <- sem(model,
            data = df06,
            missing = "ML",
-           meanstructure = T)
+           meanstructure = T, group  = "Inclusion")
 fitMeasures(fit)
 summary(fit)
 standardizedSolution(fit)
+
+
+fit <- measurementInvariance(model,
+           data = df06,
+           missing = "ML",
+           meanstructure = T, group  = "Inclusion")
+fitMeasures(fit)
+summary(fit)
+standardizedSolution(fit)
+
 
 #nImps <- 5
 #imp_method <- "pmm"
