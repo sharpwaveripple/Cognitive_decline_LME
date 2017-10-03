d5eec40c10ed0723d4cc8b6f3cff07e1948fdfcd
Jonathan Tay <jt629@medschl.cam.ac.uk>
2017-10-03 09:25:19 +0100

    cleaned scripts

--- scripts/sem.r
+++ scripts/sem.r
@@ -14,17 +14,3 @@ fit <- sem(model,
            meanstructure = T)
 fitMeasures(fit)
 
-#nImps <- 5
-#imp_method <- "pmm"
-#nIter <- 5
-#imp_data = mice(df, m = nImps, method = imp_method, seed = 1105, maxit = nIter, printFlag = FALSE)
-#imp_data_means = 0
-#for(i in 1:nImps) {
-#  imp_data_means = imp_data_means + complete(imp_data,i)
-#}
-#imp_data_means = imp_data_means/nImps
-#
-#pca <- prcomp(imp_data_means,
-#              center = T,
-#              scale = T)
-#print(pca)
