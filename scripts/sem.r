6ee96990ec6eb7e410d93d38a8a4dee0fff4ad45
estvlei <32452938+estvlei@users.noreply.github.com>
2017-10-03 10:28:37 +0100

    Update sem.r

--- scripts/sem.r
+++ scripts/sem.r
@@ -1,18 +1,28 @@
+#sem.r
 library(mice)
 library(lavaan)
 
 wd <- getwd()
-datafile <- paste(wd, "data_for_R.csv", sep="/")
-modelfile <- paste(wd, "model.lav", sep="/")
+datafile <- paste(wd, "RUNDMC_datasheet_long.csv", sep="/")
+df <- read.csv(datafile, sep=";", dec=",")
 
-df <- read.csv(datafile)
+modelfile <- paste(wd, "model.lav", sep="/")
 model <- readLines(modelfile)
 
+variables06 <- c("wmh06", "HV_06_ml_norm", "cognitiveindex06_all", 
+                 "verbalmemory06_all",	"visuospatialmemory06_all",	
+                 "immediatememory06_all",	"delayedmemory06_all",	"workingmemory06_all",	
+                 "psychomotorspeed06_all",	"fluency06_all",	"responseinhibition06_all",	
+                 "attention06_all",	"executivefunction06_all")
+df06 <- df[variables06]
+
 fit <- sem(model,
-           data = df,
+           data = df06,
            missing = "ML",
            meanstructure = T)
 fitMeasures(fit)
+summary(fit)
+standardizedSolution(fit)
 
 #nImps <- 5
 #imp_method <- "pmm"
