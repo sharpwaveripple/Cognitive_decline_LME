0a7a8734339f23401bdb3508b572ca73cd164b7b
estvlei <esther.vanleijsen@radboudumc.nl>
2017-10-06 14:19:54 +0100

    files from jess

--- scripts/model_interaction.r
+++ scripts/model_interaction.r
@@ -15,23 +15,27 @@ impute.data <- function(data, nImps, imp.method, nIter, nseed) {
   return(imp_data_means)
 }
 
-datafile <- "D:/Esther/Documenten/Werk/Neurologie/LGM_Cambridge/RUNDMC_datasheet_long.csv"
-df <- read.csv(datafile, sep=";", dec=",")
+datafile <- "data/RUNDMC_datasheet_long.csv"
+df <- read.csv(datafile, header=T)
 
 df$ps06 <- df$pp1sat06 + df$stroop1sat06 + df$stroop2sat06 + df$ldstcorrect06
 df$ps11 <- df$pp1sat11 + df$stroop1sat11 + df$stroop2sat11 + df$ldstcorrect11
 df$ps15 <- df$pp1sat15 + df$stroop1sat15 + df$stroop2sat15 + df$ldstcorrect15
 
-df$mem06 <- df$wvlt123correctmean06 + df$wvltdelayrecall06 +
-  df$reyimmrecalltotalscore06 + df$reydelayrecalltotalscore06
-df$mem11 <- df$wvlt123correctmean11 + df$wvltdelayrecall11 +
-  df$reyimmrecalltotalscore11 + df$reydelayrecalltotalscore11
-df$mem15 <- df$wvlt123correctmean15 + df$wvltdelayrecall15 +
-  df$reyimmrecalltotalscore15 + df$reydelayrecalltotalscore15
+df$mem06 <- df$wvlt123correctmean06 + df$wvltdelayrecall06 + 
+  df$reyimmrecalltotalscore06 +	df$reydelayrecalltotalscore06 +
+  df$pp2sat06 + df$pp3sat06
+df$mem11 <- df$wvlt123correctmean11 + df$wvltdelayrecall11 + 
+  df$reyimmrecalltotalscore11 +	df$reydelayrecalltotalscore11 +
+  df$pp2sat11 + df$pp3sat11
+df$mem15 <- df$wvlt123correctmean15 + df$wvltdelayrecall15 + 
+  df$reyimmrecalltotalscore15 +	df$reydelayrecalltotalscore15 +
+  df$pp2sat15 + df$pp3sat15
 
-df$exf06 <- df$fluencya06 + df$fluencyb06 + df$stroopinterference06 +   df$vsattotalsat06
-df$exf11 <- df$fluencya11 + df$fluencyb11 + df$stroopinterference11 +   df$vsattotalsat11
-df$exf15 <- df$fluencya15 + df$fluencyb15 + df$stroopinterference15 +   df$vsattotalsat15
+df$exf06 <- df$fluencyanimals06 + df$fluencyjobs06 + df$stroopinterference06 +	df$vsattotalsat06
+df$exf11 <- df$fluencyanimals11 + df$fluencyjobs11 + df$stroopinterference11 +	df$vsattotalsat11
+df$exf15 <- df$fluencysupermarket15 + df$fluencyjobs15 + df$stroopinterference15 +	df$vsattotalsat15                         
+
 
 variables.cog <- c("wmh06", "wmh11", "wmh15",
                   "hv06", "hv11", "hv15",
@@ -58,7 +62,7 @@ variables.exf <- c("wmh06", "wmh11", "wmh15",
 df.imp.exf <- df.imp[variables.exf]
 
 wd <- getwd()
-modelfile.int.ps <- paste(wd, "model_interaction.lav", sep="/")
+modelfile.int.ps <- paste(wd, "scripts/model_interaction.lav", sep="/")
 model.int.ps <- readLines(modelfile.int.ps)
 
 fit.int.ps <- growth(model.int.ps, data=df.imp.ps)
--- temp/ggplot_wmh_hv.R
+++ temp/ggplot_wmh_hv.R
@@ -1,9 +1,11 @@
 library(ggplot2)
 
-datafile_ggplot <- "../data/ggplot_dem_wmh_hv.csv"
-dem = read.csv(datafile_ggplot, sep=";", dec=",")
-attach(dem)
+setwd("D:/Esther/Documenten/Werk/Neurologie/LGM_Cambridge/RUNDMC_LGM")
 
+datafile <- "data/ggplot_dem_wmh_hv.csv"
+dem <- read.csv(datafile, sep=";", dec=",")
+
+dem.incl <- dem[dem$inclusion==1,]
 
 
 dem.wmh.age.plot <- ggplot(dem, aes(x=age, y=wmh, group=rundmcs)) +
@@ -38,3 +40,17 @@ hv.age.plot <- ggplot(dem, aes(x=age, y=hv, group=rundmcs)) +
   theme_bw(base_size=22)
 print(hv.age.plot)
 
+
+wmh.age.plot <- ggplot(dem.incl, aes(x=age, y=wmh, group=rundmcs)) +
+  geom_line() +
+  geom_point() +
+  stat_smooth(aes(group=1), method=lm, colour="#cc0000", fill="#c3c3c3", alpha=0.6) +
+  theme_bw(base_size=22)
+print(wmh.age.plot)
+
+hv.age.plot <- ggplot(dem.incl, aes(x=age, y=hv, group=rundmcs)) +
+  geom_line() +
+  geom_point() +
+  stat_smooth(aes(group=1), method=lm, colour="#cc0000", fill="#c3c3c3", alpha=0.6) +
+  theme_bw(base_size=18)
+print(hv.age.plot)
