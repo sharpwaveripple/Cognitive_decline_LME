d48e8494c121b7012fc6613e21eaa9f583992fcb
estvlei <esther.vanleijsen@radboudumc.nl>
2017-10-11 14:20:39 +0100

    restructured

---
+++ scripts/old/lgm.r
@@ -0,0 +1,13 @@
+### model.r
+library(lavaan)
+
+model <- ' # set time
+i =~ 1*t1 + 1*t2 + 1*t3
+s =~ 0*t1 + 1*t2 + 2*t3 
+'
+print(Demo.growth)
+fit <- growth(model, data=Demo.growth)
+summary(fit)
+#fname <- "RUNDMC_datasheet.csv"
+#df <- read.csv(fname, sep = ";")
+#print(names(df))
